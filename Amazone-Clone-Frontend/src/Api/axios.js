2faa871134572516c41406795131e29abd48381a
Ndg19 <158159787+Ndg19@users.noreply.github.com>
2025-09-28 15:46:54 +0300

    Remove backend submodule reference for Netlify

--- Amazone-Clone-Frontend/src/Api/axios.js
+++ Amazone-Clone-Frontend/src/Api/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  // local instance of base url
+  // baseURL: "http://localhost:5000",
+  // deployed version of amazone server on render .com
+  baseURL: " https://amazon-backend-mtyu.onrender.com",
 });
 export {axiosInstance}
